4da62b0d49e565143cd72fb207a2b7ec5b563a10
Navin1012 <nc13760@gmail.com>
2026-04-09 10:30:48 +0530

    login button

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -174,7 +174,7 @@
     <div class="d-flex flex-wrap gap-3 mt-2">
         <!-- User Login -->
         <a href="{{ route('login') }}" class="btn btn-gradient px-4 py-2">
-            <i class="fas fa-sign-in-alt me-2"></i>User Login
+            <i class="fas fa-sign-in-alt me-2"></i>Retailer Login
         </a>
 
         <!-- Supplier Login -->
